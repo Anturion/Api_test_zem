a3ecd84315b15f444cdd09a1499d56b195fb4534
Alejandro Campillo <alucardcampillo@gmail.com>
2022-01-08 12:29:16 -0500

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -9,9 +9,9 @@ BEAREAR_TWITTER_TOKEN = os.environ.get('BEAREAR_TWITTER_TOKEN')
 
 #Database settings
 
-USERNAME_DB = os.environ.get('USERNAME_DB', 'zemoga_test_db')
-PASSWORD = os.environ.get('PASSWORD', 'Zem0ga.101')
-HOST = os.environ.get('HOST', 'zemoga-test-db.crhpedy9xxto.us-east-1.rds.amazonaws.com')
-DB_NAME = os.environ.get('DB_NAME', 'zemoga_test_db')
-PORT = os.environ.get('PORT', '3306')
+USERNAME_DB = os.environ.get('USERNAME_DB')
+PASSWORD = os.environ.get('PASSWORD')
+HOST = os.environ.get('HOST')
+DB_NAME = os.environ.get('DB_NAME')
+PORT = os.environ.get('PORT')
 ID_USER = int(os.environ.get('ID_USER', '45678'))
